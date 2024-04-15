182e3191802b7ccb0e2037c918ccf3982791a261
pickett7 <45058906+pickett7@users.noreply.github.com>
2024-04-15 11:43:59 +0100

    refactor

--- vision_transformers/utils/load_weights.py
+++ vision_transformers/utils/load_weights.py
@@ -1,54 +1,16 @@
-from torch.hub import load_state_dict_from_url
+import torch
 
-urls = {
-    'vit_b_p16_224': 'https://download.pytorch.org/models/vit_b_16-c867db91.pth',
-    # 'vit_b_p16_224': 'https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_p16_224-80ecf9dd.pth',
-    'vit_b_p32_224': 'https://download.pytorch.org/models/vit_b_32-d86f8d99.pth',
-    'vit_ti_p16_224': 'https://www.dropbox.com/s/rtdzmmnod5vzc3o/vit_tiny_patch16_224.pth?dl=1',
-    'vit_ti_p16_384': 'https://www.dropbox.com/s/0848g7suf3v2h0w/vit_tiny_patch16_384.pth?dl=1'
-}
+def load_pretrained_state_dict(model, model_name='vit_ti_p16_224'):
 
-def load_pretrained_state_dict(model, model_name='vit_b_p16_224'):
-    weights = load_state_dict_from_url(urls[model_name])
-    # Model's current state dictionary.
-    state_dict = model.state_dict()
-
-    # NOTE: This loads torchvision weights.
-    if model_name == 'vit_b_p16_224' or model_name == 'vit_b_p32_224':
-        print('Loading Torchvision pretrained weights')
-        state_dict['cls_token'] = weights['class_token']
-        state_dict['pos_embedding'] = weights['encoder.pos_embedding']
-        state_dict['patches.patch.weight'] = weights['conv_proj.weight']
-        state_dict['patches.patch.bias'] = weights['conv_proj.bias']
-        
-        for i in range(12):
-            state_dict[f"transformer.layers.{i}.0.norm.weight"] = weights[f"encoder.layers.encoder_layer_{i}.ln_1.weight"]
-            state_dict[f"transformer.layers.{i}.0.norm.bias"] = weights[f"encoder.layers.encoder_layer_{i}.ln_1.bias"]
-            state_dict[f"transformer.layers.{i}.0.fn.qkv.weight"] = weights[f"encoder.layers.encoder_layer_{i}.self_attention.in_proj_weight"]
-            state_dict[f"transformer.layers.{i}.0.fn.qkv.bias"] = weights[f"encoder.layers.encoder_layer_{i}.self_attention.in_proj_bias"]
-            state_dict[f"transformer.layers.{i}.0.fn.out.0.weight"] = weights[f"encoder.layers.encoder_layer_{i}.self_attention.out_proj.weight"]
-            state_dict[f"transformer.layers.{i}.0.fn.out.0.bias"] = weights[f"encoder.layers.encoder_layer_{i}.self_attention.out_proj.bias"]
-            state_dict[f"transformer.layers.{i}.1.norm.weight"] = weights[f"encoder.layers.encoder_layer_{i}.ln_2.weight"]
-            state_dict[f"transformer.layers.{i}.1.norm.bias"] = weights[f"encoder.layers.encoder_layer_{i}.ln_2.bias"]
-            state_dict[f"transformer.layers.{i}.1.fn.mlp_net.0.weight"] = weights[f"encoder.layers.encoder_layer_{i}.mlp.linear_1.weight"]
-            state_dict[f"transformer.layers.{i}.1.fn.mlp_net.0.bias"] = weights[f"encoder.layers.encoder_layer_{i}.mlp.linear_1.bias"]
-            state_dict[f"transformer.layers.{i}.1.fn.mlp_net.3.weight"] = weights[f"encoder.layers.encoder_layer_{i}.mlp.linear_2.weight"]
-            state_dict[f"transformer.layers.{i}.1.fn.mlp_net.3.bias"] = weights[f"encoder.layers.encoder_layer_{i}.mlp.linear_2.bias"]
-            
-        state_dict['ln.weight'] = weights['encoder.ln.weight']
-        state_dict['ln.bias'] = weights['encoder.ln.bias']
-        state_dict['mlp_head.weight'] = weights['heads.head.weight']
-        state_dict['mlp_head.bias'] = weights['heads.head.bias']
-        model.load_state_dict(state_dict)
-        return model
-
+    # model_vit_tiny = torch.load('trained_models/vit_pretrained/model/vit_tiny_patch16_224.pth')
+    # print(model_vit_tiny)
     # NOTE: This loads timm weights.
-    weights = load_state_dict_from_url(urls[model_name])
+    weights = torch.load('trained_models/vit_pretrained/model/vit_tiny_patch16_224.pth')
     # Model's current state dictionary.
     state_dict = model.state_dict()
 
     if model_name == 'vit_ti_p16_224' or model_name == 'vit_ti_p16_384':
-        print('Loading timm weights')
+        print("Loading timm weight")
         state_dict['cls_token'] = weights['cls_token']
         state_dict['pos_embedding'] = weights['pos_embed']
         state_dict['patches.patch.weight'] = weights['patch_embed.proj.weight']
@@ -75,3 +37,7 @@ def load_pretrained_state_dict(model, model_name='vit_b_p16_224'):
         state_dict['mlp_head.bias'] = weights['head.bias']
         model.load_state_dict(state_dict)
         return model
+
+
+
+
